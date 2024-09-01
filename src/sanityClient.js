412c3142f2698661ed77365921109a0304559ead
Lucid Macbook <Lucid@Lucids-MacBook-Pro.local>
2024-09-01 13:41:02 +0000

    Add : Token to sanityClient

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -4,7 +4,7 @@ const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID, // replace with your project ID
   dataset: process.env.REACT_APP_SANITY_DATASET,
   apiVersion: "v2022-03-07",
-  // token: process.env.REACT_APP_SANITY_API_TOKEN,      // Optional, for authenticated requests
+  token: process.env.REACT_APP_SANITY_API_TOKEN, // Optional, for authenticated requests
   useCdn: true, // `false` if you need fresh data
 });
 
